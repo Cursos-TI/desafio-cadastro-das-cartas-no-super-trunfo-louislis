f1f8c340a2042f334d63377d8bfb9d14de39e38d
louislis <166848500+louislis@users.noreply.github.com>
2024-10-16 18:04:08 +0000

    NOME;POPULACAO;PIB;PONTOS_URISTRICOS;AREA

--- TESTE.C
+++ TESTE.C
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main (){
+char nome [50];
 int  codigo_da_cidade;
 float populacao;
 float area;
 float pib;
 int numero_de_pontos_turisticos;
 
+printf("Digite o nome\n");
+scanf("%s", &nome);
 printf("Digite o codigo_da_cidade:\n");
 scanf("%d", &codigo_da_cidade);
 printf("Digite populacao:\n");
@@ -16,8 +19,8 @@ printf("Digite pib:\n");
 scanf("%f", &pib);
 printf( "Digite numero_de_pontos_turisticos:\n");
 scanf( "%d" ,&numero_de_pontos_turisticos);
-printf("codigo_da_cidade - %d,  - populacao - %f" , codigo_da_cidade, populacao);
-printf("area - %f, pib - %f, numero_de_pontos_turisticos - %d", area, pib, numero_de_pontos_turisticos);
+printf(" nome - %s, codigo_da_cidade - %d,  - populacao - %f" , nome, codigo_da_cidade, populacao);
+printf( "area - %f, pib - %f, numero_de_pontos_turisticos - %d", area, pib, numero_de_pontos_turisticos);
 
 return 0;
 
